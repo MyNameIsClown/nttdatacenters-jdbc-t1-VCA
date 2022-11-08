a70c9be62b0e2897706d255a230bad5b2e89cb25
MyNameIsClown <96545817+MyNameIsClown@users.noreply.github.com>
2022-11-08 12:11:15 +0100

    Init

---
+++ src/main/java/com/nttdata/jdbc/App.java
@@ -0,0 +1,17 @@
+package com.nttdata.jdbc;
+
+/**
+ * App main con conexion a base de datos
+ * 
+ * @author Victor Carrasco
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		connectDDBB();
+	}
+
+	private static void connectDDBB() {
+
+	}
+}
